b1e9b104109834a2c03f1c678214acab4683674b
Solomon Aboagye <solomon.aboagye@amalitech.com>
2024-12-02 12:11:14 +0000

    fixed padding

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: PropsWithChildren) {
   const { theme } = useTheme();
   return (
     <div
-      className={`${theme} flex flex-col gap-2 bg-img-mobile md:bg-img-tablet lg:bg-img-desktop w-100 min-h-[100vh] f-grey-500 p-2 md:p-5 lg:px-20 object-cover bg-no-repeat`}
+      className={`${theme} flex flex-col gap-2 bg-img-mobile md:bg-img-tablet lg:bg-img-desktop w-100 min-h-[100vh] f-grey-500 p-2 md:p-5 lg:px-20 x-lg:px-30 object-cover bg-no-repeat`}
       id="my-root"
     >
       {children}
